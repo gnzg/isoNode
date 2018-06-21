860d26e74cc3d67e8a19e9866033f3d64f1d7801
gnzg <24455612+gnzg@users.noreply.github.com>
2018-06-21 02:19:21 +0200

    fixed z-index issue

--- src/renderImageMap.js
+++ src/renderImageMap.js
@@ -1,6 +1,7 @@
 const renderMap = (env) => {
   let drawTile;
-  let strokeStyle = '#222';
+  let strokeStyle = '#aaa';
+  let enableStroke = true;
   let { tileGraphics, map, tileW, tileH, mapX, mapY, ctx, mode, rectColors, rectShadowColors } = env;
   // loop through our map and draw out the image represented by the number.
   for (let i = 0; i < map.length; i++) {
@@ -10,35 +11,24 @@ const renderMap = (env) => {
       // two modes are possible, image-based, or rects
       if (mode === 'images') {
         ctx.drawImage(tileGraphics[drawTile], j * tileW + mapX, i * tileH + mapY);
-      } else {
-
-        ctx.globalCompositeOperation = 'source-over';
+      } else if (drawTile !== 0) {
 
         // draw all three visible sides of the rectangle
 
-        ctx.setTransform(1, -0.5, 1, 0.5, 200, 185);
-        ctx.beginPath();
-        ctx.lineWidth="1";
-        ctx.strokeStyle= strokeStyle;
-        ctx.fillStyle= rectColors[drawTile];
-        ctx.rect(mapX + j * tileW, mapY + i * tileH, tileW, tileH);
-        ctx.fillRect(j * tileW + mapX, i * tileH + mapY, tileW, tileH);
-        ctx.stroke();
-
-        ctx.globalCompositeOperation = 'destination-over';
-
-        // z axis
+        // right angle
+        ctx.globalCompositeOperation = 'source-over';
         ctx.setTransform(1, -0.5, 0, 1, 527, 70);
         ctx.beginPath();
         ctx.lineWidth="1";
         ctx.strokeStyle= strokeStyle;
         ctx.fillStyle= rectShadowColors[drawTile];
         ctx.rect(mapX + j * tileW + i * tileW, mapY + i * tileH - 2/tileW, tileW, tileH);
-        ctx.stroke();
+        enableStroke && ctx.stroke();
         ctx.fillRect(mapX + j * tileW + i * tileW, mapY + i * tileH - 2/tileW, tileW, tileH);
 
-        // x axis
-        if (j === 0) ctx.globalCompositeOperation = 'source-over';
+        // left angle
+        // was the previous element an empty tile? if so, change z-index of left side of current tile
+        ctx.globalCompositeOperation = map[i][j-1] === 0 ? 'source-over' : 'destination-over';
         ctx.setTransform(1, 0.5, 0, 1, 480, 5);
         ctx.beginPath();
         ctx.lineWidth="1";
@@ -46,10 +36,20 @@ const renderMap = (env) => {
         ctx.fillStyle= rectShadowColors[drawTile];
         // if we only care about first element of each row, set a conditional to j===0
         ctx.rect(mapX + i * tileW + tileW * j, mapY - j * tileH, tileW, tileH);
-        ctx.stroke();
+        enableStroke && ctx.stroke();
         ctx.fillRect(mapX + i * tileW + tileW * j, mapY - j * tileH, tileW, tileH);
 
-
+        // top
+        ctx.globalCompositeOperation = 'source-over';
+        ctx.setTransform(1, -0.5, 1, 0.5, 200, 185);
+        ctx.beginPath();
+        ctx.lineWidth="1";
+        ctx.strokeStyle= strokeStyle;
+        ctx.fillStyle= rectColors[drawTile];
+        ctx.rect(mapX + j * tileW, mapY + i * tileH, tileW, tileH);
+        ctx.fillRect(j * tileW + mapX, i * tileH + mapY, tileW, tileH);
+        enableStroke && ctx.stroke();
+        // debugger;
       }
     }
   }

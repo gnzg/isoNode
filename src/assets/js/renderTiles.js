1fdf410c34975802545c7ef68d62e814fe14f9b6
gnzg <24455612+gnzg@users.noreply.github.com>
2019-01-31 11:56:38 +0100

    Objectified tiles

--- src/assets/js/renderTiles.js
+++ src/assets/js/renderTiles.js
@@ -1,15 +1,8 @@
+import Tile from './tile';
 
 const renderMap = (env) => {
-  let drawTile;
-  let strokeStyle = '#333';
-  let enableStroke = true;
+
   let { tileGraphics, map, waterWorld, tileW, tileH, mapX, mapY, ctx, mode, rectColors, rectShadowColors } = env;
-  let tileYheight = 4;
-  let tileYoffset = 0;
-  let fillColor;
-  let outlineWidth = 1;
-  let tempMap = [];
-  
   // TODO: Add prevalance of elevated tiles if they rise above other tiles
   
   // clear entire canvas
@@ -18,107 +11,26 @@ const renderMap = (env) => {
   // loop through our map and draw out the image represented by the number.
   // iterator k draws the map across the y axis
   for (let k = 0; k < 9; k++) { 
-    if (k === 0) {
-      tempMap = waterWorld;
-    } else if (k === 1) {
-          tempMap = map;
-    } 
-      else {
-        tempMap = [];
-      }
-      tileYoffset = 28*k;
       // iterator i draws a row across the z axis
-      for (let i = 0; i < tempMap.length; i++) {
+      for (let i = 0; i < map.length; i++) {
         // iterator j draws a row across the x axis
         // map[i].length
-        for (let j = 0; j < tempMap[i].length; j++) {
-          drawTile = tempMap[i][j];
-          fillColor = rectShadowColors[drawTile];
-          
+        for (let j = 0; j < map[i].length; j++) {
+
           //tileYoffset = env.tileH + 4;
           //fillColor = 'red';
           
           //tileYheight= 32 * (map[i][j] === 0 ? 1 : map[i][j]);
           // to make sure tiles are drawn upwards and not downwards
           //tileYoffset = 32 * (map[i][j] === 0 ? 1 : map[i][j]);
-          
-          // two modes are possible, image-based, or rects
-          if (mode === 'images') {
-            ctx.drawImage(tileGraphics[drawTile], j * tileW + mapX, i * tileH + mapY);
-          }
-          // if not an empty tile, draw it
-          else if (drawTile !== 0) {
+         
+  
             // draw all three visible sides of the rect aspect
+            
+            let mapTile = new Tile({i:i, j:j, k:k, tileW:tileW, mapX:mapX, mapY:mapY, map:map, rectColors:rectColors, rectShadowColors:rectShadowColors, waterWorld:waterWorld});
 
-            // top
-            ctx.globalCompositeOperation = 'source-over';
+            mapTile.draw(ctx);
             
-            ctx.setTransform(1, -0.5, 1, 0.5, mapX+198, mapY+8);
-            ctx.beginPath();
-            ctx.lineWidth=outlineWidth;
-            ctx.strokeStyle= "strokeStyle";
-            // return corresponding top color based on position of fillColor in rectShadowColors[]
-            ctx.fillStyle= rectColors[rectShadowColors.indexOf(fillColor)];
-            // draw outlines        
-            ctx.rect(mapX + j * tileW + tileW - mapY + 4.665*tileW+tileYoffset,
-              mapY + i * tileW - 4.725*tileW-tileYoffset,
-              tileW,
-              tileH
-            );
-            enableStroke && ctx.stroke();
-            // fill
-            ctx.fillRect(mapX + j * tileW + tileW - mapY + 4.665*tileW+tileYoffset,
-              mapY + i * tileW - 4.725*tileW-tileYoffset,
-              tileW,
-              tileH
-            );
-
-            // right
-              ctx.globalCompositeOperation = 'source-over';
-            ctx.setTransform(1, -0.5, 0, 1, mapX+244, mapY-105);
-            ctx.beginPath();
-            ctx.lineWidth=outlineWidth;
-            ctx.strokeStyle= strokeStyle;
-            ctx.fillStyle= fillColor;
-            ctx.rect( mapX + (j + i) * tileW,
-            mapY + i * tileW-tileYoffset,
-            tileW,
-            tileH+tileYheight
-            );
-            enableStroke && ctx.stroke();
-            // fill
-            ctx.fillRect(mapX + (j + i) * tileW,
-            mapY + i * tileW-tileYoffset,
-            tileW,
-            tileH+tileYheight
-            );
-            
-            // left
-            if (j === 0 || tempMap[i][j-1] === 0) {
-              ctx.globalCompositeOperation = 'source-over';
-            } else {
-              ctx.globalCompositeOperation = 'destination-over';
-            }
-            ctx.setTransform(1, 0.5, 0, 1, mapX+220, mapY-93);
-            ctx.beginPath();
-            ctx.lineWidth=outlineWidth;
-            ctx.strokeStyle= strokeStyle;
-            ctx.fillStyle= fillColor;
-            // draw outlines
-            ctx.rect( mapX + (j + i) * tileW,
-            mapY - j * tileW - mapX - tileW-tileYoffset,
-            tileW,
-            tileH+tileYheight
-            );
-            enableStroke && ctx.stroke();
-            // fill 
-            ctx.fillRect(mapX + (j + i) * tileW,
-            mapY - j * tileW - mapX - tileW-tileYoffset,
-            tileW, tileH+tileYheight
-            );
-            
-            
-          } // end of for loop
         }
       }
     };
---
+++ src/assets/js/tile.js
@@ -0,0 +1,106 @@
+export default class Tile {
+    constructor({ x, y, tileW, fill, i, j, k, mapX, mapY, rectColors, rectShadowColors, map, waterWorld}) {
+        this.x = x || 0;
+        this.y = y || 0;
+        this.tileW = tileW;
+        this.i = i;
+        this.j = j;
+        this.k = k;
+        this.mapX = mapX || 0;
+        this.mapY = mapY || 0;
+        this.tileYoffset = 0;
+        this.rectShadowColors = rectShadowColors;
+        this.fill = fill;
+        this.waterWorld = waterWorld;
+        this.rectColors = rectColors;
+        this.fillColor = 'white';
+        this.outlineWidth = 1;
+        this.strokeStyle = '#333';
+        this.enableStroke = true;
+        this.tileYheight = 4;
+        this.map = map;
+        this.tempMap = [];
+    }
+    draw(ctx) {
+        if (this.k === 0) {
+            // alert('see tile generation');
+            this.tempMap = this.waterWorld;
+        } else if (this.k === 1) {
+            this.tempMap = this.map; //!!!
+        } else {
+            this.tempMap = [];
+        }
+        // if tile is non-zero, draw it
+        if (this.tempMap.length > 0 && this.tempMap[this.i][this.j] !== 0) {
+
+            this.tileYoffset = 48 * this.k;
+            //alert(this.tempMap[0]);
+            this.fillColor = this.rectShadowColors[this.tempMap[this.i][this.j]];
+            // top
+            ctx.globalCompositeOperation = 'source-over';
+            ctx.setTransform(1, -0.5, 1, 0.5, this.mapX+190, this.mapY);
+            ctx.beginPath();
+            ctx.lineWidth=this.outlineWidth;
+            ctx.strokeStyle= this.strokeStyle;
+            // return corresponding top color based on position of fillColor in rectShadowColors[]
+            ctx.fillStyle= this.rectColors[this.rectShadowColors.indexOf(this.fillColor)];
+            // draw outlines        
+            ctx.rect(this.mapX + this.j * this.tileW + this.tileW - this.mapY + 4.665*this.tileW+this.tileYoffset,
+                this.mapY + this.i * this.tileW - 4.725*this.tileW-this.tileYoffset,
+                this.tileW,
+                this.tileW
+            );
+            this.enableStroke && ctx.stroke();
+            // fill
+            ctx.fillRect(this.mapX + this.j * this.tileW + this.tileW - this.mapY + 4.665*this.tileW+this.tileYoffset,
+                this.mapY + this.i * this.tileW - 4.725*this.tileW-this.tileYoffset,
+                this.tileW,
+                this.tileW
+            );
+                 
+            // right
+            ctx.globalCompositeOperation = 'source-over';
+            ctx.setTransform(1, -0.5, 0, 1, this.mapX+236, this.mapY-113);
+            ctx.beginPath();
+            ctx.lineWidth=this.outlineWidth;
+            ctx.strokeStyle= this.strokeStyle;
+            ctx.fillStyle= this.fillColor;
+            ctx.rect( this.mapX + (this.j + this.i) * this.tileW,
+            this.mapY + this.i * this.tileW-this.tileYoffset,
+            this.tileW,
+            this.tileW+this.tileYheight
+            );
+            this.enableStroke && ctx.stroke();
+            // fill
+            ctx.fillRect(this.mapX + (this.j + this.i) * this.tileW,
+            this.mapY + this.i * this.tileW-this.tileYoffset,
+            this.tileW,
+            this.tileW+this.tileYheight
+            );
+      
+            // left
+            if (this.j === 0 || this.tempMap[this.i][this.j-1] === 0) {
+                ctx.globalCompositeOperation = 'source-over';
+            } else {
+                ctx.globalCompositeOperation = 'destination-over';
+            }
+            ctx.setTransform(1, 0.5, 0, 1, this.mapX+212, this.mapY-101);
+            ctx.beginPath();
+            ctx.lineWidth=this.outlineWidth;
+            ctx.strokeStyle= this.strokeStyle;
+            ctx.fillStyle= this.fillColor;
+            // draw outlines
+            ctx.rect( this.mapX + (this.j + this.i) * this.tileW,
+            this.mapY - this.j * this.tileW - this.mapX - this.tileW-this.tileYoffset,
+            this.tileW,
+            this.tileW+this.tileYheight
+            );
+            this.enableStroke && ctx.stroke();
+            // fill 
+            ctx.fillRect(this.mapX + (this.j + this.i) * this.tileW,
+            this.mapY - this.j * this.tileW - this.mapX - this.tileW-this.tileYoffset,
+            this.tileW, this.tileW+this.tileYheight
+            );
+        } 
+    }
+}

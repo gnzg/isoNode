d59d4502b4f58c7defd441cc1f90768ac88ba4f2
gnzg <24455612+gnzg@users.noreply.github.com>
2019-07-13 10:58:36 +0200

    adding tests...

--- tests/homebrewStore.test.js
+++ tests/homebrewStore.test.js
@@ -4,3 +4,15 @@ test('Add an item to our homebrew store', () => {
   store.dispatch('addItem', 'foo');
   expect(store.state.misc[0] === 'foo').toBe(true);
 });
+
+
+test('Should rotate the ground map', () => {
+  let beforeRotationX = store.state.env.map[0].length;
+  let beforeRotationZ = store.state.env.map.length;
+  store.dispatch('rotateMap', store.state);
+  let afterRotationX = store.state.env.map[0].length;
+  let afterRotationZ = store.state.env.map.length;
+  expect(beforeRotationX === afterRotationZ &&
+         beforeRotationZ === afterRotationX
+    ).toBe(true);
+});

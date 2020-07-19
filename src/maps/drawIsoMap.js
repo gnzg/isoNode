314ff56d9d9759ded8bd2b67b6ac932dd3cdce82
gnzg <24455612+gnzg@users.noreply.github.com>
2020-07-19 11:49:36 +0200

    refactoring

--- src/maps/drawIsoMap.js
+++ src/maps/drawIsoMap.js
@@ -126,33 +126,31 @@ export default ({ctx, maps, mapX, mapY, tileWidth, y, x, mapIndex, rectColors, r
       }
 
       // top
-      
-      // check placement based on tile offset 
-      // if current offset is larger than the offset of the next tile, 
-      // then current tile visibility dominates
-      
-      ctx.fillStyle = tile.rectColor;
-      
-      if (
-        // should be 4 cases
-        (tempMap[y  + 1] !== undefined && mapHeight[y][x] >= mapHeight[y+1][x]) && (mapHeight[y][x+1] < mapHeight[y][x])
-        || (mapHeight[y][x] == mapHeight[y][x+1])
-      ) { 
-        ctx.globalCompositeOperation = 'source-over';
-      } else {
-        ctx.globalCompositeOperation = 'destination-over';
-      }
-      
-      ctx.beginPath();
-      ctx.moveTo(tile.tileWidth * y + tile.tileWidth + mapX + tile.tileWidth * x, tile.tileWidth + topYsegment);
-      ctx.lineTo(tile.tileWidth * y  + tile.tileWidth * 2 + mapX + tile.tileWidth * x, d + topYsegment);
-      ctx.lineTo(tile.tileWidth * y  + tile.tileWidth + mapX + tile.tileWidth * x, tile.tileWidth * 2 + topYsegment);
-      ctx.lineTo(tile.tileWidth * y  + tile.tileWidth - tile.tileWidth + mapX + tile.tileWidth * x, d + topYsegment);
-      ctx.closePath();
-      ctx.fill();
-      
-
-
+      // draw only if current tile is non-zero
+      if (tempMap[y][x] !== 0) {
+        ctx.fillStyle = tile.rectColor;
+        
+        // check placement based on tile offset 
+        // if current offset is larger than the offset of the next tile, 
+        // then current tile visibility dominates
+        if (
+          // should be 4 cases
+          (tempMap[y + 1] !== undefined && 
+            mapHeight[y][x] >= mapHeight[y+1][x])
+            ) { 
+              ctx.globalCompositeOperation = 'source-over';
+            } else {
+              ctx.globalCompositeOperation = 'destination-over';
+            }
+            
+            ctx.beginPath();
+            ctx.moveTo(tile.tileWidth * y + tile.tileWidth + mapX + tile.tileWidth * x, tile.tileWidth + topYsegment);
+            ctx.lineTo(tile.tileWidth * y  + tile.tileWidth * 2 + mapX + tile.tileWidth * x, d + topYsegment);
+            ctx.lineTo(tile.tileWidth * y  + tile.tileWidth + mapX + tile.tileWidth * x, tile.tileWidth * 2 + topYsegment);
+            ctx.lineTo(tile.tileWidth * y  + tile.tileWidth - tile.tileWidth + mapX + tile.tileWidth * x, d + topYsegment);
+            ctx.closePath();
+            ctx.fill();
+    }
         
       // draw vertices; only available in debug mode
       if (state.debug_mode === true) {

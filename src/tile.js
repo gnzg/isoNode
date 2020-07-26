6ce7f36999bf9ca882d41f786d8b2fac700997e9
gnzg <24455612+gnzg@users.noreply.github.com>
2020-07-26 15:54:47 +0200

    refactoring tile

--- src/tile.js
+++ src/tile.js
@@ -13,14 +13,15 @@ export default class Tile {
             this.x = x;
             this.y = y;
             
-            this.c = state.env.mapY - this.tileWidth * x * 0.5;
+            this.tileWidth = state.env.tileWidth;
+
+            this.c = state.env.mapY - this.tileWidth * this.x * 0.5;
             this.d = this.tileWidth * 1.5;
             
             /* size */
-            this.tileWidth = state.env.tileWidth;
             
-            this.tileYoffset = this.tileWidth * tileHeightMap[y][x] * 1.25 + map[y][x]*10;
-            this.topYfactor = this.tileWidth * y * 0.5;
+            this.tileYoffset = this.tileWidth * tileHeightMap[this.y][this.x] * 1.25 + map[this.y][this.x]*10;
+            this.topYfactor = this.tileWidth * this.y * 0.5;
             this.topYsegment = this.c + this.topYfactor - this.tileYoffset;
 
             this.rectColors = state.env.rectColors;
@@ -38,7 +39,7 @@ export default class Tile {
             this.style = style || this.defaultStyle;
             */
             
-            this.fillColor = this.rectShadowColors[map[y][x]];
+            this.fillColor = this.rectShadowColors[map[this.y][this.x]];
             this.rectColor = this.rectColors[this.rectShadowColors.indexOf(this.fillColor)];
         } else {
             console.error("The tile object received too many/few arguments!");

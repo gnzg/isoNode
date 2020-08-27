95d9bdb647138f4474ca26750c473837bcfbdb94
gnzg <24455612+gnzg@users.noreply.github.com>
2020-08-28 00:11:40 +0200

    refactoring

--- src/store/mutations/refreshCanvas.js
+++ src/store/mutations/refreshCanvas.js
@@ -19,13 +19,14 @@ export default (state) => {
     if (map !== undefined) {
       for (let y = 0; y < map.length; y++) {
         for (let i = 0; i < state.maxTileHeight; i++) {
-        for (let x = 0; x < map[y].length; x++) {
+          for (let x = 0; x < map[y].length; x++) {
             // draw first z level first, then the next one and so on
             // based on z-height
             drawTileLaterals({ tile: new Tile({ x, y }), x, y, z:i });
             drawTileTop({ tile: new Tile({ x, y }), x, y, z:i });
           }
         }
+      // debugger;
       }
     } else {
       console.error("no maps object found!");

d98e9673abe2b14587689ec3e31541a2936e446f
gnzg <24455612+gnzg@users.noreply.github.com>
2021-07-13 20:21:11 +0200

    UPDATE: Makefile improvements

--- isValidPort.sh
+++ isValidPort.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# convert the first parameter to an integer
 export INT=$(expr $1 + 0)
-if [ $INT -gt 999 ]; then
+
+# throw an error if no parameter is supplied
+if [ "$#" -ne 1 ]; then
+  echo 'Please supply a valid port value, e.g. PORT=1337'
+  exit 1
+elif [ $INT -gt 999 ]; then
   exit 1
 else
-  echo $1 'is an invalid port value. A value between 1000 and 9999 is advisable.'
+  echo $1 'A value between 1000 and 9999 is advisable.'
 fi

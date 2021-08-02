05a91ba41389dc281a2b0aa2b1693725e82cdc22
gnzg <24455612+gnzg@users.noreply.github.com>
2021-08-02 12:22:51 +0200

    FIX: centerCanvas.js error message

--- src/store/mutations/centerCanvas.js
+++ src/store/mutations/centerCanvas.js
@@ -2,14 +2,12 @@ import Store from "../index";
 
 export default function (state) {
   if (state.env.map_tiles !== undefined && Array.isArray(state.env.map_tiles)) {
-    // set new window boundaries
-    //store.state.env.winWidth = window.innerWidth;
-    //store.state.env.winHeight = window.innerHeight;
 
     let mapCenter = (state.env.map_tiles.length / 2) * state.env.tileWidth * 2.3;
     state.env.map_offset_x = state.env.winWidth / 2 - mapCenter;
     state.env.map_offset_y = state.env.winHeight / 2;
 
+  } else {
+    Store.dispatch("error", "Invalid state.env.map_tiles value!");
   }
-  Store.dispatch("error", "Invalid state.env.map_tiles value!");
 }

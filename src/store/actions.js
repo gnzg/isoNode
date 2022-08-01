09d80af64e5503a14aee5e47340d0ac94ce8767f
Ilya Ginzburg <24455612+gnzg@users.noreply.github.com>
2022-08-01 22:20:40 +0200

    Updated dependencies; removed eslint; added prettier

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,48 +1,48 @@
 export default {
-  error(context, payload) {
-    context.commit('error', payload);
-  },
-  addEnvProp(context, payload) {
-    context.commit('addEnvProp', payload);
-  },
-  centerCanvas(context) {
-    context.commit('centerCanvas');
-  },
-  handleKeyDown(context, payload) {
-    context.commit('handleKeyDown', payload);
-  },
-  handleKeyUp(context, payload) {
-    context.commit('handleKeyUp', payload);
-  },
-  updateCanvas(context) {
-    context.commit('updateCanvas');
-  },
-  rotateMap(context) {
-    context.commit('rotateMap');
-  },
-  deleteTile(context, payload) {
-    context.commit('deleteTile', payload);
-  },
-  onTileHover(context, payload) {
-    context.commit('onTileHover', payload);
-  },
-  onTileUnhover(context, payload) {
-    context.commit('onTileUnhover', payload);
-  },
-  saveLastHoveredTile(context, payload) {
-    context.commit('saveLastHoveredTile', payload);
-  },
-  checkCollision(context, payload) {
-    context.commit('checkCollision', payload);
-  },
-  toggleDebugMode(context) {
-    context.commit('toggleDebugMode');
-    context.commit('updateCanvas');
-  },
-  createTileHitBoxes(context) {
-    context.commit('createTileHitBoxes');
-  },
-  clearTileHitBoxes(context) {
-    context.commit('clearTileHitBoxes');
-  }
-};
+    error(context, payload) {
+        context.commit('error', payload)
+    },
+    addEnvProp(context, payload) {
+        context.commit('addEnvProp', payload)
+    },
+    centerCanvas(context) {
+        context.commit('centerCanvas')
+    },
+    handleKeyDown(context, payload) {
+        context.commit('handleKeyDown', payload)
+    },
+    handleKeyUp(context, payload) {
+        context.commit('handleKeyUp', payload)
+    },
+    updateCanvas(context) {
+        context.commit('updateCanvas')
+    },
+    rotateMap(context) {
+        context.commit('rotateMap')
+    },
+    deleteTile(context, payload) {
+        context.commit('deleteTile', payload)
+    },
+    onTileHover(context, payload) {
+        context.commit('onTileHover', payload)
+    },
+    onTileUnhover(context, payload) {
+        context.commit('onTileUnhover', payload)
+    },
+    saveLastHoveredTile(context, payload) {
+        context.commit('saveLastHoveredTile', payload)
+    },
+    checkCollision(context, payload) {
+        context.commit('checkCollision', payload)
+    },
+    toggleDebugMode(context) {
+        context.commit('toggleDebugMode')
+        context.commit('updateCanvas')
+    },
+    createTileHitBoxes(context) {
+        context.commit('createTileHitBoxes')
+    },
+    clearTileHitBoxes(context) {
+        context.commit('clearTileHitBoxes')
+    },
+}
--- src/store/mutations/checkCollision.ts
+++ src/store/mutations/checkCollision.ts
@@ -1,78 +1,90 @@
-import StateInterface from "../../interfaces/StateInterface";
-import pointInRhombus from "../../math/PointInRhombus";
-import store from "../index";
+import StateInterface from '../../interfaces/StateInterface'
+import pointInRhombus from '../../math/PointInRhombus'
+import store from '../index'
 
 // check whether cursor coordinates fall within saved hitboxes of non-empty tiles
 
-export default (state: StateInterface, payload : MouseEvent) => {
+export default (state: StateInterface, payload: MouseEvent) => {
+    // initial check if required hitboxes exist
+    if (state.map_data.tileHitBoxes.length <= 0) {
+        store.dispatch('error', 'tileHitBoxes length is zero! Recreating...')
+        store.dispatch('createTileHitBoxes')
+    }
 
-  // initial check if required hitboxes exist
-  if (state.map_data.tileHitBoxes.length <= 0) {
-    store.dispatch("error", "tileHitBoxes length is zero! Recreating...");
-    store.dispatch("createTileHitBoxes");
-  }
+    let cursor_pos_x = payload.clientX
+    let cursor_pos_y = payload.clientY
+    let tileCoordinates = state.map_data.tileHitBoxes
+    let lastHoveredTile = store.state.map_data.lastHoveredTile
 
-  let cursor_pos_x = payload.clientX;
-  let cursor_pos_y = payload.clientY;
-  let tileCoordinates = state.map_data.tileHitBoxes;
-  let lastHoveredTile = store.state.map_data.lastHoveredTile;
+    // TODO: re-write; current approach is expensive
+    for (let i = 0; i < tileCoordinates.length; i++) {
+        // if cursor is within a given tile's space
+        if (
+            pointInRhombus(tileCoordinates[i], {
+                x: cursor_pos_x,
+                y: cursor_pos_y,
+            })
+        ) {
+            console.log('foo')
 
-  // TODO: re-write; current approach is expensive
-  for (let i = 0; i < tileCoordinates.length; i++) {
+            // on initial run, save first hovered tile as lastHoveredTile
+            // or, if currentlyHoveredTile does not correspond to the current tile coordinates, update the former
+            store.state.map_data.currentlyHoveredTile = {
+                x: tileCoordinates[i].x,
+                y: tileCoordinates[i].y,
+            }
 
-    // if cursor is within a given tile's space
-    if (pointInRhombus(tileCoordinates[i], { x: cursor_pos_x, y: cursor_pos_y })) {
-      
-      // on initial run, save first hovered tile as lastHoveredTile
-      // or, if currentlyHoveredTile does not correspond to the current tile coordinates, update the former
-        store.state.map_data.currentlyHoveredTile = {
-          x: tileCoordinates[i].x,
-          y: tileCoordinates[i].y,
-        };
+            store.dispatch(
+                'saveLastHoveredTile',
+                store.state.map_data.currentlyHoveredTile
+            )
+            store.dispatch(
+                'onTileHover',
+                store.state.map_data.currentlyHoveredTile
+            )
+            store.dispatch('updateCanvas')
 
-        store.dispatch("saveLastHoveredTile", store.state.map_data.currentlyHoveredTile);
-        store.dispatch("onTileHover", store.state.map_data.currentlyHoveredTile);
-        store.dispatch("updateCanvas");
-      
-        // if currentlyHoveredTile is different than lastHoveredTile, unhover the former
-      if (store.state.map_data.lastHoveredTile.x != tileCoordinates[i].x) {
-        store.state.map_data.currentlyHoveredTile = {
-          x: tileCoordinates[i].x,
-          y: tileCoordinates[i].y,
-        };
+            // if currentlyHoveredTile is different than lastHoveredTile, unhover the former
+            if (
+                store.state.map_data.lastHoveredTile.x != tileCoordinates[i].x
+            ) {
+                store.state.map_data.currentlyHoveredTile = {
+                    x: tileCoordinates[i].x,
+                    y: tileCoordinates[i].y,
+                }
 
-        store.dispatch("onTileUnhover", state.map_data.lastHoveredTile);
-        store.dispatch("onTileHover", store.state.map_data.currentlyHoveredTile);
-        store.dispatch("updateCanvas");
-      }
+                store.dispatch('onTileUnhover', state.map_data.lastHoveredTile)
+                store.dispatch(
+                    'onTileHover',
+                    store.state.map_data.currentlyHoveredTile
+                )
+                store.dispatch('updateCanvas')
+            }
 
-
-
-
-      // only unhover lastHoveredTile if it was already set
-      /*
+            // only unhover lastHoveredTile if it was already set
+            /*
       if (state.map_data.lastHoveredTile.x !== undefined) {
         store.dispatch("onTileUnhover", state.map_data.lastHoveredTile);
       }
       */
 
-      // if hovering a new tile
-      if (
-        lastHoveredTile.x !== state.map_data.lastHoveredTile.x ||
-        lastHoveredTile.y !== state.map_data.lastHoveredTile.y) {
-        store.dispatch("onTileUnhover", state.map_data.lastHoveredTile);
+            // if hovering a new tile
+            if (
+                lastHoveredTile.x !== state.map_data.lastHoveredTile.x ||
+                lastHoveredTile.y !== state.map_data.lastHoveredTile.y
+            ) {
+                store.dispatch('onTileUnhover', state.map_data.lastHoveredTile)
 
-        //store.dispatch("onTileHover", lastHoveredTile);
-        //store.dispatch("saveLastHoveredTile", lastHoveredTile);
-        //store.dispatch("updateCanvas");
-      }
-     
-    }
-    // TODO: if mouse is outside of map -> calculate map size based on location and tile max length in both x,z axes
-    /*else if (!pointInRhombus(tileCoordinates[i], { x: cursor_pos_x, y: cursor_pos_y })) {
+                //store.dispatch("onTileHover", lastHoveredTile);
+                //store.dispatch("saveLastHoveredTile", lastHoveredTile);
+                //store.dispatch("updateCanvas");
+            }
+        }
+        // TODO: if mouse is outside of map -> calculate map size based on location and tile max length in both x,z axes
+        /*else if (!pointInRhombus(tileCoordinates[i], { x: cursor_pos_x, y: cursor_pos_y })) {
       TODO: store.dispatch('info', 'cursor is outside of map!');
     }*/
-  }
-  payload.stopImmediatePropagation();
-  return state;
-};
+    }
+    payload.stopImmediatePropagation()
+    return state
+}
